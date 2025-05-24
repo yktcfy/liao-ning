1f40cb33fdae6c1acffa2dd21a504b7551e342ff
yktcfy <zjyktcfy@qq.com>
2025-05-24 12:42:50 +0800

    Update and rename zubo2.sh to CaijiLiaoningip.sh

--- CaijiLiaoningip.sh
+++ CaijiLiaoningip.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]; then
   echo "开始测试······"
   echo "在5秒内输入1~4可选择城市"
   echo "请选择城市："
-  echo "1. 辽宁联通"
+  echo "1. 辽宁（Liaoning_liantong）"
   read -t 5 -p "超时未输入,将按默认设置测试" city_choice
 
   if [ -z "$city_choice" ]; then
@@ -17,7 +17,7 @@ fi
 # 设置城市和相应的stream
 case $city_choice in
     1)
-        city="辽宁联通"
+        city="Liaoning_liantong"
         stream="rtp/232.0.0.25:1234"
         ;;
     0)
